95c8f3fb667c577e74017cf7785cd9747f5ea2ff
Arka <arkapatra2009@gmail.com>
2023-01-16 00:08:29 +0530

    ENV Variables

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get('/',(req, res) => {
 
 app.get('/welcome', (req,res) => {
     res.send('<html><h1>Welcome Page</h1></html>');
-});
+}); 
 
 app.listen('5000', ()=> console.log("Express Server running at port 5000"));
