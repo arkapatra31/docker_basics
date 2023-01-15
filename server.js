c4200722619a171246857726ed0cdca878c98263
Arka <arkapatra2009@gmail.com>
2023-01-15 22:51:58 +0530

    CHANGES

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.get('/',(req, res) => {
     res.send("Index Page on Docker");
 });
 
-app.listen('5000');
+app.get('/welcome', (req,res) => {
+    res.send('<html><h1>Welcome Page</h1></html>');
+});
+
+app.listen('5000', ()=> console.log("Express Server running at port 5000"));
